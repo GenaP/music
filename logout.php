e4b667a5c1118853b4979dcce707dffd7d5b196e
Gena <gena_luboml@ukr.net>
2014-12-30 16:52:52 +0200

    Status and delete account

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 session_start();
 unset($_SESSION['user_id']);
 unset($_SESSION['user_nick']);
+unset($_SESSION['user_status']);
 session_destroy();
 echo"<meta http-equiv=refresh content='1; url=index.php'><center><h1>Logout complete</h1>";
 ?>
